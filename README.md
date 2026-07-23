d490f574385e112b2927f10a23b129acfaae3ed7
Trần Vân Anh <tranvananh@Van-Anh-he-he-he.local>
2026-07-23 20:01:34 +0700

    Them file README va anh UML

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ứng dụng Quản lý Chi tiêu Cá nhân
+
+Đây là dự án Bài tập lớn môn OOP.
+
+## Sơ đồ kiến trúc lớp (Class Diagram)
+![Sơ đồ UML](docs/class-diagram.png)
---
+++ src/main/java/com/example/personalexpensemanager/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.personalexpensemanager;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
